afa58c5004549212a0ba8b756931ab2139333073
Stacy Read <sread@wctc.edu>
2021-12-06 17:10:02 -0600

    Changed annoying JDK

--- src/edu/wctc/wisco/PaymentProcessor.java
+++ src/edu/wctc/wisco/PaymentProcessor.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PaymentProcessor {
-    private List<Payment> paymentList = new ArrayList<>();
+    private final List<Payment> paymentList = new ArrayList<>();
 
     public void addPayment(Payment payment) {
         paymentList.add(payment);
     }
 
     public double totalByMonth(int month) {
-        return paymentList.stream().filter(p -> p.getPaymentDate().getMonthValue() == month)
+        return paymentList.stream()
+                .filter(p -> p.getPaymentDate().getMonthValue() == month)
                 .mapToDouble(Payment::getAmount)
                 .sum();
     }
